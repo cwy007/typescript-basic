f8dd5b4b98c14d653765ceea83f8c03c13361874
Viking Zhang <vikingmute@gmail.com>
2020-05-02 15:49:42 +0800

    2-13 add generics part 2

--- generics.ts
+++ generics.ts
@@ -8,3 +8,22 @@ function swap<T, U>(tuple: [T, U]): [U, T] {
   return [tuple[1], tuple[0]]
 }
 const result2 = swap(['string', 123])
+
+function echoWithArr<T>(arg: T[]): T[] {
+  console.log(arg.length)
+  return arg
+}
+const arrs = echoWithArr([1, 2, 3])
+
+interface IWithLength {
+  length: number
+}
+
+function echoWithLength<T extends IWithLength>(arg: T): T {
+  console.log(arg.length)
+  return arg
+}
+
+const str = echoWithLength('str')
+const obj = echoWithLength({ length: 10, width: 10})
+const arr2 = echoWithLength([1, 2, 3])
