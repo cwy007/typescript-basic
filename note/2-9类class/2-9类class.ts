48196b848751d836b5d6c329e9484e1aef614fed
chenweiyan <chanweiyan007@gmail.com>
2020-12-07 18:39:17 +0800

    2-9 class

---
+++ note/2-9类class/2-9类class.ts
@@ -0,0 +1,15 @@
+// js 构造函数，原型链
+// es6
+// 类 class
+// class 定义了一切事物的抽象特点
+// object 类的实例
+// oop特征，封装，继承，多态
+//
+// class extends constructor static super
+
+// typescript 修饰符，定义属性和方法的访问形式
+// public  共有的
+// private 私有的，自能在 class 中被调用，子类中也不能访问
+// protected 修饰的属性和方法和被保护的，在类中和子类中可以被调用
+
+// 只能读 readonly 修饰符
